d660bf33b62e46e7b3f14439ae5d006b4a3aa8b0
Leejeongbin01 <ljb011013@gmail.com>
2025-07-24 23:26:08 +0900

    25-07-24

---
+++ 완전탐색/19532.cpp
@@ -0,0 +1,16 @@
+#include <tream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a,b,c,d,e,f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+
+
+    return 0;
+}
